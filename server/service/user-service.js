b717518c622a4c592c831e73903977aeaf479f1d
Emil Arzumanov <22-emil@mail.ru>
2023-03-16 21:03:17 +0500

    error handler and login are ready

--- server/service/user-service.js
+++ server/service/user-service.js
@@ -4,22 +4,48 @@ const uuid = require("uuid");
 const mailService = require("./mail-service");
 const tokenService = require("./token-service")
 const UserDto = require("../dtos/user-dto");
+const ApiError = require('../exceptions/api-error');
 
 class UserService {
     async registration(email, password) {
         const candidate = await UserModel.findOne({email})
         if (candidate) {
-            throw new Error(`Пользователь с потовым адресом ${email} уже существует`);
+            throw ApiError.BadRequest(`Пользователь с потовым адресом ${email} уже существует`);
         }
         const hashPassword = await bcrypt.hash(password, 3);
         const activationLink = uuid.v4();
         const user = await UserModel.create({email, password:hashPassword, activationLink});
-        await mailService.sendActivationMail(email, activationLink);
+        await mailService.sendActivationMail(email, `${process.env.API_URL}/api/activate/${activationLink}`);
 
         const userDto = new UserDto(user); //id, email, isActivated
         const tokens = tokenService.generateToken({...userDto});
         await tokenService.saveToken(userDto.id, tokens.refreshToken);
 
+        return {...tokens, user: userDto}
+    }
+
+    async activate(activationLink) {
+        const user = await UserModel.findOne({activationLink})
+        if (!user) {
+            throw ApiError.BadRequest("Неккоректная ссылка активации");
+        }
+        user.isActivated = true;
+        await user.save();
+    }
+
+    async login(email, password) {
+        const user = await UserModel.findOne({email})
+        if (!user) {
+            throw ApiError.BadRequest('Пользователь с таким email не найден');
+        }
+        const isPassEquals = await bcrypt.compare(password, user.password);
+        if (!isPassEquals) {
+            throw ApiError.BadRequest('Неверный пароль');
+        }
+        const userDto = new UserDto(user);
+        const tokens = tokenService.generateToken({...userDto});
+
+        await tokenService.saveToken(userDto.id, tokens.refreshToken);
         return {...tokens, user: userDto}
     }
 }
